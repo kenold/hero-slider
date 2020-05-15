d2c3441b79fe4a25408542de2af4a44013753edf
Kenold Beauplan <something4ken@gmail.com>
2020-05-15 12:23:33 -0400

    create sample data

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+const data = [{
+        title: "PDX Women in tech scholarships go all virtual",
+        image: "zrEkb4ILymM",
+        category: "React",
+        cta: "Explore more",
+        url: "https://google.com"
+    },
+    {
+        title: "Women in tech face the challenge of not being taken seriously",
+        image: "fch6vkbouCc",
+        category: "Javascript",
+        cta: "Find out more",
+        url: "https://github.com"
+    }
+];
